71d7131ed07ac613fdc4efbc6458c9c7a05209ae
Борис Зискин <bmziskin@gmail.com>
2013-09-25 13:23:49 +0400

    DoctrineMigrationsBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,8 +20,10 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
 
             new Vifeed\CampaignBundle\CampaignBundle(),
+
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
