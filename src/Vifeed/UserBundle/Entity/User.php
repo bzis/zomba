e21d0406dda2b0b5c2f2d95986ea3fcc5d8d43c8
Борис Зискин <bmziskin@gmail.com>
2013-10-01 16:30:20 +0400

    изменение сущности юзера

--- src/Vifeed/UserBundle/Entity/User.php
+++ src/Vifeed/UserBundle/Entity/User.php
@@ -4,6 +4,7 @@ namespace Vifeed\UserBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use FOS\UserBundle\Model\User as BaseUser;
+use Symfony\Component\Validator\Constraints as Assert;
 
 
 /**
@@ -23,6 +24,19 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", columnDefinition="ENUM('advertiser', 'publisher')")
+     *
+     * @Assert\Choice(
+     *      choices = {"advertiser", "publisher"},
+     *      groups={"default"},
+     *      message = "Выберите тип"
+     * )
+     */
+    protected $type;
+
 
     /**
      * Get id
@@ -32,5 +46,21 @@ class User extends BaseUser
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param mixed $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
     }
 }
