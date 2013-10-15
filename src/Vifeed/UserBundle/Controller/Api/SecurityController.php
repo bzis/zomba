a1a48453dd76ed9a9c609aa571503b3edc60f447
Dmitry Tsoy <hd.deman@gmail.com>
2013-10-16 00:26:41 +0400

    stage frontend

--- src/Vifeed/UserBundle/Controller/Api/SecurityController.php
+++ src/Vifeed/UserBundle/Controller/Api/SecurityController.php
@@ -3,11 +3,6 @@
 namespace Vifeed\UserBundle\Controller\Api;
 
 use FOS\RestBundle\Controller\FOSRestController;
-use FOS\RestBundle\View\View;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 /**
  * Class SecurityController
@@ -16,5 +11,8 @@ use Nelmio\ApiDocBundle\Annotation\ApiDoc;
  */
 class SecurityController extends FOSRestController
 {
+    public function userRegisterAction()
+    {
 
+    }
 }
