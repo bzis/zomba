c33514ddb45fa7a165ecdffa83711ac0815a7594
Борис Зискин <bmziskin@gmail.com>
2013-10-18 19:29:50 +0400

    убрана прямая связь кампании и площадки

--- src/Vifeed/CampaignBundle/Form/CampaignType.php
+++ src/Vifeed/CampaignBundle/Form/CampaignType.php
@@ -26,7 +26,7 @@ class CampaignType extends AbstractType
               ->add('endAt', 'datetime')
               ->add('totalViews')
               ->add('bid')
-              ->add('platforms')
+//              ->add('platforms')
               ->add('countries')
               ->add('tags')
               ->add('ageRanges');
