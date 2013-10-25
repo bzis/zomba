aa5ed3830d7dd731120fb30479179595b97910bf
Борис Зискин <bmziskin@gmail.com>
2013-10-26 01:53:57 +0400

    тест на получение юзера по id

--- src/Vifeed/UserBundle/Controller/Api/UserController.php
+++ src/Vifeed/UserBundle/Controller/Api/UserController.php
@@ -6,6 +6,8 @@ use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\View\View;
 use Symfony\Component\HttpFoundation\Response;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Vifeed\UserBundle\Entity\User;
 
 
 /**
@@ -30,6 +32,12 @@ class UserController extends FOSRestController
     {
         $userManager = $this->container->get('fos_user.user_manager');
         $user = $userManager->findUserBy(['id' => $id]);
+
+        var_dump($id);
+        if (!$user instanceof User) {
+            throw new NotFoundHttpException('User not found');
+        }
+
         $view = new View(array(
                               'user' => $user,
                          ));
