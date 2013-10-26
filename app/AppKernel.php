602f5c3b9b9b9f60e4a1044ef2a5eb16040e5c40
Dmitry Tsoy <hd.deman@gmail.com>
2013-10-26 21:53:39 +0400

    now we can use symfony routes in js over FOSJsRoutingBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,6 +25,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Sensio\Bundle\BuzzBundle\SensioBuzzBundle(),
             new Snc\RedisBundle\SncRedisBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
 
             new Vifeed\CampaignBundle\VifeedCampaignBundle(),
             new Vifeed\UserBundle\VifeedUserBundle(),
