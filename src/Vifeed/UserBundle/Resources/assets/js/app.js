19f838b43d16384511b6f312b4b5a9d46649d65d
Dmitry Tsoy <hd.deman@gmail.com>
2013-10-27 22:28:59 +0400

    user auth frontend

---
+++ src/Vifeed/UserBundle/Resources/assets/js/app.js
@@ -0,0 +1,28 @@
+// (function () {
+//   'use strict';
+
+//   // private variables for mock server
+//   var apiAuthUrl = '/api/user/login_check', //Routing.generate('api_fos_user_security_check'), //'/api/auth',
+//     _authenticated = false,
+//     _username = null,
+//     _id = null;
+
+//   // angular.module('authtest', ['http-auth-interceptor', 'authentication'], function($interpolateProvider) {
+//   //   $interpolateProvider.startSymbol('{[{');
+//   //   $interpolateProvider.endSymbol('}]}');
+// });
+
+var userApp = angular.module('userApp', ['http-auth-interceptor', 'authentication'], function($interpolateProvider) {
+    $interpolateProvider.startSymbol('{[{');
+    $interpolateProvider.endSymbol('}]}');
+}).config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/sign-in', {
+            templateUrl: '/bundles/vifeeduser/partials/sign-in.html', 
+            controller: SigninController
+        })
+        .when('/sign-up', {
+            templateUrl: '/bundles/vifeeduser/partials/sign-up.html', 
+            controller: SignupController
+        });
+}]);
