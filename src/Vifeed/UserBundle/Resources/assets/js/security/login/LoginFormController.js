7c7275d29789681e8aca6de93a19765c1cee0a84
Dmitry Tsoy <hd.deman@gmail.com>
2013-11-15 02:40:22 +0400

    new auth pattern

---
+++ src/Vifeed/UserBundle/Resources/assets/js/security/login/LoginFormController.js
@@ -0,0 +1,44 @@
+angular.module('security.login.form', ['services.localizedMessages'])
+
+// The LoginFormController provides the behaviour behind a reusable form to allow users to authenticate.
+// This controller and its template (login/form.tpl.html) are used in a modal dialog box by the security service.
+.controller('LoginFormController', ['$scope', 'security', 'localizedMessages', function($scope, security, localizedMessages) {
+  // The model for this form 
+  $scope.user = {};
+
+  // Any error message from failing to login
+  $scope.authError = null;
+
+  // The reason that we are being asked to login - for instance because we tried to access something to which we are not authorized
+  // We could do something diffent for each reason here but to keep it simple...
+  $scope.authReason = null;
+  if ( security.getLoginReason() ) {
+    $scope.authReason = ( security.isAuthenticated() ) ?
+      localizedMessages.get('login.reason.notAuthorized') :
+      localizedMessages.get('login.reason.notAuthenticated');
+  }
+
+  // Attempt to authenticate the user specified in the form's model
+  $scope.login = function() {
+    // Clear any previous security errors
+    $scope.authError = null;
+
+    // Try to login
+    security.login($scope.user.email, $scope.user.password).then(null, function(response) {
+      if(response.status === 401) {
+          $scope.authError = localizedMessages.get('login.error.invalidCredentials');
+      } else {
+          // If we get here then there was a problem with the login request to the server
+          $scope.authError = localizedMessages.get('login.error.serverError', { exception: response.data.message });
+      }
+    });
+  };
+
+  $scope.clearForm = function() {
+    $scope.user = {};
+  };
+
+  $scope.cancelLogin = function() {
+    security.cancelLogin();
+  };
+}]);
