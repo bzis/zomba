5b50465c33d7fcfa9e08b9ce4e2715f6da7f19de
Dmitry Tsoy <hd.deman@gmail.com>
2013-12-29 16:01:22 +0400

    remove mapping to Country entity

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -36,8 +36,7 @@ class AppKernel extends Kernel
             new Vifeed\SystemBundle\VifeedSystemBundle(),
             new Vifeed\PlatformBundle\VifeedPlatformBundle(),
             new Vifeed\PaymentBundle\VifeedPaymentBundle(),
-            new Vifeed\FrontendBundle\VifeedFrontendBundle(),
-            new Vifeed\GeoBundle\VifeedGeoBundle()
+            new Vifeed\FrontendBundle\VifeedFrontendBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
