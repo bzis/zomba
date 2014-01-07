979c0dc7b8dbb314e92f577d940ab44715a04f9d
Dmitry Tsoy <hd.deman@gmail.com>
2014-01-07 05:49:15 +0400

    install js and css depedencies via bower

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -66,6 +66,7 @@ end
 
 before 'symfony:assetic:dump' do
     run "sh -c 'cd #{latest_release} && npm install'"
+    run "sh -c 'cd #{latest_release} && bower install'"
     run "sh -c 'cd #{latest_release} && grunt'"
 end
 
