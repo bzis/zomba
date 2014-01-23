71342629da88bbb5d533b0593d6517815c59eb17
Dmitry Tsoy <hd.deman@gmail.com>
2014-01-23 19:59:19 +0400

    update supervisord after deploy

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -23,6 +23,7 @@ default_run_options[:pty] = false
 # Automatically set proper permissions
 # http://capifony.org/cookbook/set-permissions.html
 set :writable_dirs,       [app_path + '/cache', app_path + '/logs']
+set :shared_children,     [app_path + '/config/supervisord']
 set :webserver_user,      'www-data'
 set :permission_method,   :acl
 set :use_set_permissions, true
@@ -37,7 +38,6 @@ set :composer_options,  '--verbose --prefer-dist --optimize-autoloader'
 
 set :keep_releases,  3
 
-
 set :dump_assetic_assets, true
 set :assets_install,      true
 
@@ -47,33 +47,35 @@ logger.level = Logger::MAX_LEVEL
 deploy.start
 
 namespace :deploy do
-  task :start, :roles => :app, :except => { :no_release => true } do
+  task :start, roles: :app, except: { no_release: true } do
   end
-  task :stop, :roles => :app, :except => { :no_release => true } do
+  task :stop, roles: :app, except: { no_release: true } do
   end
-  task :restart, :roles => :app, :except => { :no_release => true } do
+  task :restart, roles: :app, except: { no_release: true } do
     run 'sudo /etc/init.d/nginx restart'
     puts '--> Restarting nginx'.green
     run 'sudo /etc/init.d/php5-fpm restart'
     puts '--> Restarting php5-fpm'.green
     # run 'sudo service varnish restart'
     # puts '--> Restarting varnish'.green
-    # run 'sudo supervisorctl update'
-    # puts '--> Updating supervisord commands'.green
-    # run 'sudo supervisorctl start all'
-    # puts '--> Starting supervisord commands'.green
+    run 'sudo supervisorctl update'
+    puts '--> Updating supervisord commands'.green
+    run 'sudo supervisorctl start all'
+    puts '--> Starting supervisord commands'.green
   end
 end
 
+before 'symfony:cache:warmup', 'symfony:doctrine:migrations:migrate'
+
 before 'symfony:assetic:dump' do
-    run "sh -c 'cd #{latest_release} && npm install'"
-    run "sh -c 'cd #{latest_release} && bower install'"
-    run "sh -c 'cd #{latest_release} && php app/console fos:js-routing:dump --env=prod'"
-    run "sh -c 'cd #{latest_release} && grunt'"
+  run "sh -c 'cd #{latest_release} && npm install'"
+  run "sh -c 'cd #{latest_release} && bower install'"
+  run "sh -c 'cd #{latest_release} && php app/console fos:js-routing:dump --env=prod'"
+  run "sh -c 'cd #{latest_release} && grunt'"
 end
 
 after 'symfony:assetic:dump' do
-    run "sh -c 'cd #{latest_release} && grunt s3'"
+  run "sh -c 'cd #{latest_release} && grunt s3'"
 end
 
 set :parameters_dir, 'app/config/parameters'
