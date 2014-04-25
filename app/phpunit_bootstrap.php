1f8560cc72c7080a53d0afad6d1e0d82b8590e4f
Борис Зискин <bmziskin@gmail.com>
2014-04-25 15:17:46 +0400

    все миграции запускаются перед тестами

--- app/phpunit_bootstrap.php
+++ app/phpunit_bootstrap.php
@@ -4,13 +4,13 @@ require_once __DIR__ . '/bootstrap.php.cache';
 
 require_once __DIR__ . '/AppKernel.php';
 
-use Doctrine\Bundle\DoctrineBundle\Command\Proxy\CreateSchemaDoctrineCommand;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Input\ArrayInput;
 use Doctrine\Bundle\DoctrineBundle\Command\DropDatabaseDoctrineCommand;
 use Doctrine\Bundle\DoctrineBundle\Command\CreateDatabaseDoctrineCommand;
 use Doctrine\Bundle\FixturesBundle\Command\LoadDataFixturesDoctrineCommand;
+use Doctrine\Bundle\MigrationsBundle\Command\MigrationsMigrateDoctrineCommand;
 
 $kernel = new AppKernel('test', true);
 $kernel->boot();
@@ -27,8 +27,8 @@ if ($result !== false) {
     $command = new DropDatabaseDoctrineCommand();
     $application->add($command);
     $input = new ArrayInput(array(
-                                 'command' => 'doctrine:database:drop',
-                                 '--force' => true,
+                                  'command' => 'doctrine:database:drop',
+                                  '--force' => true,
                             ));
     $command->run($input, new ConsoleOutput());
     if ($connection->isConnected()) {
@@ -39,21 +39,22 @@ if ($result !== false) {
 $command = new CreateDatabaseDoctrineCommand();
 $application->add($command);
 $input = new ArrayInput(array(
-                             'command' => 'doctrine:database:create',
+                              'command' => 'doctrine:database:create',
                         ));
 $command->run($input, new ConsoleOutput());
 
-$command = new CreateSchemaDoctrineCommand();
+$command = new MigrationsMigrateDoctrineCommand();
 $application->add($command);
 $input = new ArrayInput(array(
-                             'command' => 'doctrine:schema:create',
+                              'command' => 'doctrine:migrations:migrate',
                         ));
+$input->setInteractive(false);
 $command->run($input, new ConsoleOutput());
 
 $command = new LoadDataFixturesDoctrineCommand();
 $application->add($command);
 $input = new ArrayInput(array(
-                             'command' => 'doctrine:fixtures:load',
+                              'command' => 'doctrine:fixtures:load',
                         ));
 $input->setInteractive(false);
 $command->run($input, new ConsoleOutput());
