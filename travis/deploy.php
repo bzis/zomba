fb962f518bdb803ee6f2ffe5ae88531b226cc7ab
Борис Зискин <bmziskin@gmail.com>
2014-04-27 01:52:33 +0400

    дебаг

--- travis/deploy.php
+++ travis/deploy.php
@@ -1,10 +1,11 @@
 #!/usr/bin/php
 <?php
 var_dump($_SERVER['TRAVIS_PULL_REQUEST']);
-if ($_SERVER['TRAVIS_BRANCH'] == 'master' && $_SERVER['TRAVIS_PULL_REQUEST'] !== 'true') {
+if ($_SERVER['TRAVIS_BRANCH'] == 'master' && $_SERVER['TRAVIS_PULL_REQUEST'] == 'false') {
     $GIT_USER = `git log -1 \$TRAVIS_COMMIT --pretty=format:"%an"`;
     $GIT_EMAIL = `git log -1 \$TRAVIS_COMMIT --pretty=format:"%ae"`;
-    echo `git config user.name --global "$GIT_USER"`;
+    echo `git config user.name "$GIT_USER" --global `;
+    echo `git config user.name`;
     echo `git config user.email "$GIT_EMAIL"`;
     echo `gem install bundle`;
     echo `bundle install`;
