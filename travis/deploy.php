a34e0f60e3f72269e1710b948fc52399db7e2644
Борис Зискин <bmziskin@gmail.com>
2014-04-27 01:16:10 +0400

    ещё попытка деплоя после билда

--- travis/deploy.php
+++ travis/deploy.php
@@ -4,9 +4,9 @@
 if ($_SERVER['TRAVIS_BRANCH'] == 'master' && !$_SERVER['TRAVIS_PULL_REQUEST']) {
     $GIT_USER = `git log -1 \$TRAVIS_COMMIT --pretty=format:"%an"`;
     $GIT_EMAIL = `git log -1 \$TRAVIS_COMMIT --pretty=format:"%ae"`;
-    `git config user.name --global $GIT_USER`;
-    `git config user.email $GIT_EMAIL`;
-    `gem install bundle`;
-    `bundle install`;
-    `bundle exec cap staging deploy`;
+    echo `git config user.name --global $GIT_USER`;
+    echo `git config user.email $GIT_EMAIL`;
+    echo `gem install bundle`;
+    echo `bundle install`;
+    echo `bundle exec cap staging deploy`;
 }
