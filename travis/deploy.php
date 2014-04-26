b465811d065dc719cd4bd5503db64b10cebe3f28
Борис Зискин <bmziskin@gmail.com>
2014-04-27 01:30:51 +0400

    пофиксил php и вернул sh

--- travis/deploy.php
+++ travis/deploy.php
@@ -1,11 +1,11 @@
 #!/usr/bin/php
 <?php
 
-if ($_SERVER['TRAVIS_BRANCH'] == 'master' && !$_SERVER['TRAVIS_PULL_REQUEST']) {
+if ($_SERVER['TRAVIS_BRANCH'] == 'master' && $_SERVER['TRAVIS_PULL_REQUEST'] !== 'true') {
     $GIT_USER = `git log -1 \$TRAVIS_COMMIT --pretty=format:"%an"`;
     $GIT_EMAIL = `git log -1 \$TRAVIS_COMMIT --pretty=format:"%ae"`;
-    echo `git config user.name --global $GIT_USER`;
-    echo `git config user.email $GIT_EMAIL`;
+    echo `git config user.name --global "$GIT_USER"`;
+    echo `git config user.email "$GIT_EMAIL"`;
     echo `gem install bundle`;
     echo `bundle install`;
     echo `bundle exec cap staging deploy`;
