e9657846c9bb37b83a5f6bf87047b420c6b1c48c
Dmitry Tsoy <hd.deman@gmail.com>
2014-05-07 19:34:59 +0400

    disable png for imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -101,7 +101,7 @@ module.exports = function(grunt) {
         files: [{
           expand: true,                  // Enable dynamic expansion
           cwd: 'web/bundles/',                   // Src matches are relative to this path
-          src: ['**/*.{png,jpg,gif}']   // Actual patterns to match
+          src: ['**/*.{jpg,gif}']   // Actual patterns to match
         }]
       }
     },
