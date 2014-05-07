7dfcfa3894c6a0709fd7900a0c5938da06538a69
Dmitry Tsoy <hd.deman@gmail.com>
2014-05-07 16:34:20 +0400

    fix Warning: spawn ENOENT

--- Gruntfile.js
+++ Gruntfile.js
@@ -100,7 +100,7 @@ module.exports = function(grunt) {
       dynamic: {                         // Another target
         files: [{
           expand: true,                  // Enable dynamic expansion
-          cwd: './web/bundles/',                   // Src matches are relative to this path
+          cwd: 'web/bundles/',                   // Src matches are relative to this path
           src: ['**/*.{png,jpg,gif}']   // Actual patterns to match
         }]
       }
