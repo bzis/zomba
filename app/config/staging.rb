7766a599d1d5c14cb5298da27cb292d1d91fc260
Dmitry Tsoy <hd.deman@gmail.com>
2014-05-28 13:43:41 +0400

    fix log permissions if it was created on deploy stage

--- app/config/staging.rb
+++ app/config/staging.rb
@@ -2,5 +2,5 @@ server 'vifeed.co', :app, :web, :primary => true
 set :parameters_file, 'parameters_staging.yml'
 
 after :deploy do
-  run "sh -c 'cd #{latest_release} && test -f app/logs/prod.log || touch app/logs/prod.log && chown vagrant:www-data app/logs/* && chmod 664 app/logs/*'"
+  run "sh -c 'cd #{latest_release} && test -f app/logs/prod.log || touch app/logs/prod.log && chown deploy:www-data app/logs/* && chmod 664 app/logs/*'"
 end
