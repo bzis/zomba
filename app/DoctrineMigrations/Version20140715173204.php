51b94ca5d70573662db523912e576abb126deb88
Борис Зискин <bmziskin@gmail.com>
2014-07-15 22:39:15 +0400

    фикс по миграции

--- app/DoctrineMigrations/Version20140715173204.php
+++ app/DoctrineMigrations/Version20140715173204.php
@@ -14,9 +14,9 @@ class Version20140715173204 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
-        
-        $this->addSql("CREATE TABLE company (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, system ENUM('ОСН', 'УСН') NOT NULL, name VARCHAR(100) NOT NULL, contact_name VARCHAR(50) NOT NULL, role VARCHAR(50) NOT NULL, reason VARCHAR(50) NOT NULL, details VARCHAR(500) NOT NULL, address VARCHAR(500) NOT NULL, phone VARCHAR(30) NOT NULL, UNIQUE INDEX UNIQ_4FBF094FA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB");
-        $this->addSql("ALTER TABLE company ADD CONSTRAINT FK_4FBF094FA76ED395 FOREIGN KEY (user_id) REFERENCES vifeed_user (id)");
+
+        $this->addSql("CREATE TABLE company (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, system ENUM('ОСН', 'УСН') NOT NULL, name VARCHAR(100) NOT NULL, contact_name VARCHAR(50) DEFAULT NULL, role VARCHAR(50) DEFAULT NULL, reason VARCHAR(50) DEFAULT NULL, details VARCHAR(500) DEFAULT NULL, address VARCHAR(500) DEFAULT NULL, phone VARCHAR(30) DEFAULT NULL, UNIQUE INDEX UNIQ_4FBF094FA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB");
+        $this->addSql("ALTER TABLE company ADD CONSTRAINT FK_4FBF094FA76ED395 FOREIGN KEY (user_id) REFERENCES vifeed_user (id) ON DELETE CASCADE");
     }
 
     public function down(Schema $schema)
